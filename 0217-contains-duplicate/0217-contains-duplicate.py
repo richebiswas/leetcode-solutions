c38d0e289369cfa343d3bdcc7c98369f1396eb32
Krittika Biswas <richebiswas@gmail.com>
2026-09-09 01:07:03 +0530

    Time: 31 ms (41.21%), Space: 25.8 MB (92.19%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        dit = set()
+        for i in nums:
+            if i in dit:
+                return True
+            dit.add(i)
+            
+        return False 
